148559fc8ab891372400563aab8e1e454b7fbde7
«Natalia <kolodina_melbyk@mail.ru>
2023-02-14 19:56:53 +0400

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 25. Напишите цикл, который принимает на вход 2 числа (А и В) и возводит число А в натуралтную степень В.
+// 3, 5 -> 243 (3 в 5)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}, {b} -> {RaiseToPow(a, b)}");
+
+int RaiseToPow(int num1, int num2)
+{
+    int result = 1;
+    while (num2 > 0)
+    {
+        result = result * num1;
+        num2--;
+    }
+    return result;
+}
